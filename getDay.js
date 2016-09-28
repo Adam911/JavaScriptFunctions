a83673773b2f03735e43b675f9c8d60e4a33dd2c
Dwayne <saulsdwayne@gmail.com>
2016-09-28 14:43:25 +0200

    Updated

---
+++ getDay.js
@@ -0,0 +1,7 @@
+ var getDay = function()
+ var givendate = '2016-09-11';
+
+ var date = new Date(givendate);
+
+ console.log(date.getDay()+ "It falls on a Sunday");
+//Hardcoded string based on the date inputted
